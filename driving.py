ad2e620d6da30efc1f4bed504c80675d6f917ac7
Kawa Araki <qq86853130pp@gmail.com>
2020-09-11 18:41:37 +0800

    add USA

--- driving.py
+++ driving.py
@@ -3,6 +3,11 @@ age = input("請輸入年齡：")
 age = int(age)
 if country == "台灣":
     if age >= 18:
-    	print("你可以合法做愛")
+    	print("你可以考駕照")
     else:
-    	print("你做愛會被抓")
+    	print("你還不能考駕照")
+elif country == "美國":
+	if age >= 16:
+		print("你可以考駕照")
+	else:
+		print("你還不能考駕照")
